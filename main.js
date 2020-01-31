795fece20da3ac9e5955df987b8933b65705a6cd
lobana alarabi <loubanaeng@gmail.com>
2020-01-31 20:21:45 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
         
-        let wri=document.getElementById('201811189').
+        
+        
+        let wri=document.quer('201811189').
         wri.className="activated-heart";
       }
     }, 300);
